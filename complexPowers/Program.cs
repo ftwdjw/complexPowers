b5e116cd6bdb9fb8165d0b89ef1bd23480a3e79b
ftwdjw <ftwdjw@gmail.com>
2018-03-05 15:43:44 -0600

    works

---
+++ complexPowers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace complexPowers
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
