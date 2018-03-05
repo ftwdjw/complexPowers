c6ee8afb9546be652ed1979f6dc143f6b26af3b2
ftwdjw <ftwdjw@gmail.com>
2018-03-05 16:01:00 -0600

    works

--- complexPowers/Program.cs
+++ complexPowers/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
+
 namespace complexPowers
 {
     class MainClass
     {
+        public struct complex  
+        {  
+            public double r;  
+            public double j; 
+        }  
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            complex c1 = new complex();
+
+            c1.r = 0.0;
+            c1.j = 0.0;
+
+            Console.WriteLine("r={0:N},j={1:N}",c1.r,c1.j);
         }
     }
 }
